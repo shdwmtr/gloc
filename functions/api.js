5e2e5dc4fdec9a94ce5b9f0c9e04b9b000be1f10
shadow <81448108+ShadowMonster99@users.noreply.github.com>
2024-06-14 23:42:49 -0300

    impl(core): init commit

---
+++ functions/api.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const Router = express()
+const PORT = 3000
+
+const CalculateLinesOfCode = (json) => {
+  let lines = 0
+
+  for (const i in json)
+  {
+    if (json[i].language == 'C Header' || json[i].language == "C++")
+      lines += json[i].linesOfCode
+  }
+
+  return {
+    schemaVersion: 1,
+    label: "lines",
+    message: String(lines)
+  }
+}
+
+Router.get('/', (request, response) => 
+{
+  fetch("https://api.codetabs.com/v1/loc/?github=SteamClientHomebrew/Millennium&branch=main&ignored=vendor")
+  .then(text => text.json())
+  .then(json => {
+    response.json(CalculateLinesOfCode(json))
+  })
+})
+
+Router.listen(PORT, () => console.log(`listening on ${PORT}`))
